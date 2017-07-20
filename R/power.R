11c6953f854966376de476f936992b14a5b98382
tinoswe <olivomartino@gmail.com>
2017-07-20 09:58:50 +0200

    add all

---
+++ R/power.R
@@ -0,0 +1,19 @@
+#' Square a number
+#'
+#' Takes in any numeric value and squares it.
+#' @param x A numeric value to be squared
+#' @return The square of the input
+#' @export
+square <- function(x){
+  return(x^2)
+}
+
+#' Cube a number
+#'
+#' Takes in any numeric value and cubes it.
+#' @param x A numeric value to be cubed
+#' @return The cube of the input
+#' @export
+cube <- function(x){
+  return(x^3)
+}
